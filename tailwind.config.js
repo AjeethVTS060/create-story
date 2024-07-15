b0d4911d9588d1743d489a51f302d3af7efae616
AjeethVTS060 <158513382+AjeethVTS060@users.noreply.github.com>
2024-07-15 23:54:25 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
   prefix: '',
   important: false,
   separator: ':',
